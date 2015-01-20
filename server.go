103450d99e3e8cca9a4c7a7b7f6dfc8b244c0fe7
Peter Bengtsson <mail@peterbe.com>
2015-01-19 20:14:52 -0800

    switch to redigo

--- server.go
+++ server.go
@@ -6,9 +6,9 @@ import (
 	"github.com/unrolled/render"
 	"os"
 	// "github.com/gorilla/mux"
-	"github.com/fzzy/radix/redis"
+	// "github.com/fzzy/radix/redis"
 	// "github.com/xuyu/goredis"
-	// "github.com/garyburd/redigo/redis"
+	"github.com/garyburd/redigo/redis"
 	"net/http"
 	// "time"
 	// "log"
@@ -84,8 +84,10 @@ func main() {
 
 	c, err := redis.Dial("tcp", "localhost:6379")
 	errHndlr(err)
-	r := c.Cmd("select", 9)
-	errHndlr(r.Err)
+	defer c.Close()
+	c.Do("SELECT", 9)
+	// errHndlr(err)
+	// fmt.Println("database_select", database_select)
 
 	// // Using redigo
 	// c, err := redis.Dial("tcp", ":6379")
@@ -94,6 +96,9 @@ func main() {
 	// }
 	// defer c.Close()
 	// c.Do("SELECT", 9)
+	r, err := redis.String(c.Do("GET", "fookey"))
+	errHndlr(err)
+	fmt.Println("R", r)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
@@ -103,12 +108,24 @@ func main() {
 		q := strings.Trim(req.FormValue("q"), " ")
 
 		terms := CleanWords(q)
+		// fmt.Println("TERMS", terms)
 		// NOTE! Maybe we don't need the ZINTERSTORE if there's only 1 command
-		c.Append("ZINTERSTORE", "$tmp", len(terms), terms, "AGGREGATE", "max")
-		c.Append("ZREVRANGE", "$tmp", 0, 10, "WITHSCORES")
-
-		c.GetReply() // the ZINTERSTORE
-		replies, err := c.GetReply().List()
+		args := make([]interface{}, len(terms) + 4)
+		args[0] = "$tmp"
+		args[1] = len(terms)
+		for i, term := range terms {
+			args[i+2] = term
+		}
+		args[len(terms)+2] = "AGGREGATE"
+		args[len(terms)+3] = "MAX"
+		// fmt.Println("ARGS", args)
+		c.Send("ZINTERSTORE", args...)
+		c.Send("ZREVRANGE", "$tmp", 0, 10, "WITHSCORES")
+		c.Flush()
+		c.Receive() // the ZINTERSTORE
+		// replies, err := c.Receive()
+		replies, err := redis.Strings(redis.Values(c.Receive()))
+		// fmt.Println("replies", replies, len(replies))
 		errHndlr(err)
 
 		ooids := make([]string, 10+1)
@@ -116,10 +133,10 @@ func main() {
 		evens := 0
 		for i, element := range replies {
 			if i%2 == 0 {
-				ooids[evens] = element
+				ooids[evens] = string(element)
 				evens = evens + 1
 			} else {
-				scores[evens-1] = element
+				scores[evens-1] = string(element)
 			}
 		}
 		ooids = ooids[:evens]
@@ -129,9 +146,15 @@ func main() {
 		if len(ooids) == 0 {
 			// titles = [1]string{}
 		} else {
-			titles, err = c.Cmd("HMGET", "$titles", ooids).List()
+			args := make([]interface{}, len(ooids)+1)
+			args[0] = "$titles"
+			for i, v := range ooids {
+				args[i+1] = v
+			}
+			// fmt.Println("args", args)
+			titles, err = redis.Strings(c.Do("HMGET", args...))
 			errHndlr(err)
-
+			// titles, err = redis.Strings(reply, err)
 		}
 		rows := make([]interface{}, len(titles))
 		for i, title := range titles {
