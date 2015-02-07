a30119e143e08afd91ac338b456986d810232d2a
Peter Bengtsson <mail@peterbe.com>
2015-02-07 13:48:45 -0800

    destination option

--- sampleloader/populate.py
+++ sampleloader/populate.py
@@ -1,3 +1,4 @@
+import os
 import json
 import hashlib
 
@@ -8,22 +9,24 @@ import redis
 
 _base = 'http://localhost:3000'
 key = hashlib.md5(open(__file__).read()).hexdigest()
+_here = os.path.dirname(__file__)
+
 
 def get_blogposts():
-    items = json.load(open('blogposts.json'))['items']
+    items = json.load(open(os.path.join(_here, 'blogposts.json')))['items']
     for i, item in enumerate(items):
         url = 'http://www.peterbe.com/plog/%s' % item['slug']
         yield (item['title'], url, len(items) - i)
 
 
-def populate(database, domain, flush=False):
+def populate(database, destination, domain, flush=False):
     c = redis.StrictRedis(host='localhost', port=6379, db=database)
     if flush:
         c.flushdb()
     c.hset('$domainkeys', key, domain)
 
     for title, url, popularity in get_blogposts():
-        _url = _base + '/v1'
+        _url = destination + '/v1'
         data = {
             'title': title,
             'url': url,
@@ -41,11 +44,12 @@ def populate(database, domain, flush=False):
 
 @click.command()
 @click.option('--database', '-d', default=8)
+@click.option('--destination', default='http://autocompeter.com')
 @click.option('--domain', default='autocompeter.com')
 @click.option('--flush', default=False, is_flag=True)
-def run(database, domain, flush=False):
+def run(database, destination, domain, flush=False):
     #print (database, domain, flush)
-    populate(database, domain, flush=flush)
+    populate(database, destination, domain, flush=flush)
 
 if __name__ == '__main__':
     run()
