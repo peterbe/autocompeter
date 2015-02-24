2ec9477d2ea2f8b3f6bf8f682314f8034eaa8463
Peter Bengtsson <mail@peterbe.com>
2015-02-24 12:27:51 -0800

    test travis running server

---
+++ travis-e2e.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go build server.go authkeys.go
+./server -port=3000 -redisDatabase=8 &
+# I wish a I knew a way to install python things without sudo
+sudo pip install nose redis requests
+nosetests
