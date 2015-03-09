1c244edbb3ff3864b96540300d833c6dc8cafbd3
Peter Bengtsson <mail@peterbe.com>
2015-03-09 09:16:38 -0700

    e2e update

--- travis-e2e.sh
+++ travis-e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go build server.go authkeys.go
+go build server.go authkeys.go api.go
 ./server -port=3000 -redisDatabase=8 &
 # I wish a I knew a way to install python things without sudo
 sudo pip install nose redis requests
