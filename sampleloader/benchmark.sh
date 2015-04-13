f78a94bc16a0363f7142744795871b5484540b37
Peter Bengtsson <mail@peterbe.com>
2015-04-13 08:49:08 -0700

    remove double benchmark

--- sampleloader/benchmark.sh
+++ sampleloader/benchmark.sh
@@ -16,5 +16,3 @@ echo "Double word search 'python', 'te'"
 wrk -c 10 -d5 -t5 "$URL/v1?q=python%20te&d=mydomain" | grep Requests
 echo "Double word search 'xxxxxxxx', 'yyyyyyy'"
 wrk -c 10 -d5 -t5 "$URL/v1?q=xxxxxxxx%20yyyyyy&d=mydomain" | grep Requests
-echo "Double word search 'xxxxxxxx', 'yyyyyyy'"
-wrk -c 10 -d5 -t5 "$URL/v1?q=xxxxxxxx%20yyyyyy&d=mydomain" | grep Requests
