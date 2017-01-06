66dcda64c15a2c4367104bdcb69190fa18a122e0
Peter Bengtsson <mail@peterbe.com>
2017-01-05 20:26:03 -0500

    correction in wsgi

--- autocompeter/wsgi.py
+++ autocompeter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_autocompeter project.
+WSGI config for autocompeter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_autocompeter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autocompeter.settings")
 
 application = get_wsgi_application()
